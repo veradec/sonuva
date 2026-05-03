f713aeca7c827f029ca3877779f8a325a8788748
veradec <glitchmill.dyj3d@simplelogin.co>
2026-05-03 20:48:11 +0530

    feat: logging

---
+++ README.md
@@ -0,0 +1,9 @@
+# sonuva
+
+sonuva is a c helper single header library 
+
+feat:
+
+[ ] String Tokenizer 
+[ ] File I/O
+
--- main.c
+++ main.c
@@ -1,10 +1,26 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdarg.h>
+
+void LOG_INFO(char *str) { printf("+ [INFO] %s\n", str); }
+
+void LOG_LEVEL(char *str, char *level) {
+  if (strcmp(level, "warn") == 0) {
+    printf("! [WARN] %s\n", str);
+    return;
+  }
+
+  if (strcmp(level, "fatal") == 0) {
+    printf("!! [FATAL] %s\n", str);
+  }
+}
 
 
-
-// Driver Code
 int main()
 {
-  
+  LOG_INFO("This is a Log");
+  LOG_LEVEL("This is a Warning", "warn");
+  /* LOG("HELLO"); */
   return 0;
 }
